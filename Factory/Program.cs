e7de6bfc22ad6453f9f6bc9b04bc01e4cccdb6d0
Kin <150116082+Kinema233@users.noreply.github.com>
2023-12-18 17:32:40 +0200

    Add files via upload

---
+++ Factory/Program.cs
@@ -0,0 +1,60 @@
+﻿//предметна область бібліотеки FunLib
+
+using System;
+
+// Інтерфейс для представлення книг
+interface IBook
+{
+    void Display();
+}
+
+// Конкретні класи книг
+class FantasyBook : IBook
+{
+    public void Display()
+    {
+        Console.WriteLine("Fantasy Book");
+    }
+}
+
+class ScienceFictionBook : IBook
+{
+    public void Display()
+    {
+        Console.WriteLine("Science Fiction Book");
+    }
+}
+
+// Фабрика для створення різних видів книг
+class BookFactory
+{
+    public IBook CreateBook(string genre)
+    {
+        switch (genre.ToLower())
+        {
+            case "fantasy":
+                return new FantasyBook();
+            case "sciencefiction":
+                return new ScienceFictionBook();
+            default:
+                throw new ArgumentException("Unknown genre");
+        }
+    }
+}
+
+// Приклад використання фабрики
+class Program
+{
+    static void Main()
+    {
+        BookFactory bookFactory = new BookFactory();
+
+        // Створюємо книгу жанру "Фентезі"
+        IBook fantasyBook = bookFactory.CreateBook("fantasy");
+        fantasyBook.Display();
+
+        // Створюємо книгу жанру "Наукова фантастика"
+        IBook scienceFictionBook = bookFactory.CreateBook("sciencefiction");
+        scienceFictionBook.Display();
+    }
+}
